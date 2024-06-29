6faaf11aa6599a59cc128a2e80b054641cac95ee
Pazaz <pazaz@itspazaz.com>
2024-06-28 21:56:21 -0400

    feat: Initial commit

---
+++ src/main/java/Version.java
@@ -0,0 +1,12 @@
+import org.openrs2.deob.annotation.OriginalClass;
+import org.openrs2.deob.annotation.OriginalMember;
+
+@OriginalClass("mapview!p")
+public class Version {
+
+    @OriginalMember(owner = "mapview!p", name = "a", descriptor = "[I")
+    public static final int[] field152 = new int[] { 118, -62, 7, 24, 121, -123, -10, -8, 116, 6, -106, 28, -36, 86, 109, 112, -115, -116, -37, -35 };
+
+    @OriginalMember(owner = "mapview!p", name = "b", descriptor = "I")
+    public static int field153;
+}
