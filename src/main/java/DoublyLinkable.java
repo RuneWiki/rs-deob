33feb5a4bdb65ca72d81800a774e610f29e81671
Pazaz <pazaz@itspazaz.com>
2024-06-28 21:56:14 -0400

    feat: Initial commit

---
+++ src/main/java/DoublyLinkable.java
@@ -0,0 +1,9 @@
+import org.openrs2.deob.annotation.OriginalClass;
+import org.openrs2.deob.annotation.OriginalMember;
+
+@OriginalClass("mapview!d")
+public class DoublyLinkable extends Linkable {
+
+    @OriginalMember(owner = "mapview!d", name = "d", descriptor = "I")
+    public static int field36;
+}
---
+++ src/main/java/Version.java
@@ -0,0 +1,12 @@
+import org.openrs2.deob.annotation.OriginalClass;
+import org.openrs2.deob.annotation.OriginalMember;
+
+@OriginalClass("mapview!p")
+public class Version {
+
+    @OriginalMember(owner = "mapview!p", name = "a", descriptor = "[I")
+    public static final int[] field152 = new int[] { -76, -59, 89, 83, -53, 36, 21, 50, -12, 116, 1, 109, 64, -69, 1, -86, -13, -42, 94, -62 };
+
+    @OriginalMember(owner = "mapview!p", name = "b", descriptor = "I")
+    public static int field153;
+}
